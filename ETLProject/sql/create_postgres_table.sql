fa50019c95e1aab9328f0c0f6c168fdb2af00cb6
Diego Noguera Maciel <53942598+Dieg0Maciel@users.noreply.github.com>
2023-08-27 18:59:06 -0300

    Delete create_postgres_table.sql

--- ETLProject/sql/create_postgres_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS hourly_forecast(
-    request_time  VARCHAR(50),
-    forecast_time  VARCHAR(25),
-    description  VARCHAR(100),
-    temperature  FLOAT(8),
-    pressure  INT,
-    humidity  INT,
-    wind_speed  FLOAT(8)
-) 
